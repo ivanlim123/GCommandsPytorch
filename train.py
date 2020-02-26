6f558cd806987a386cb3e1ce0f3cee11526c7fe5
Ivan Lim <36753941+ivanlim123@users.noreply.github.com>
2020-02-26 17:48:57 +0800

    Save Model

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 import torch.nn.functional as F
 from torch.autograd import Variable
-
+import torch
 
 def train(loader, model, optimizer, epoch, cuda, log_interval, verbose=True):
     model.train()
@@ -15,12 +15,12 @@ def train(loader, model, optimizer, epoch, cuda, log_interval, verbose=True):
         loss = F.nll_loss(output, target)
         loss.backward()
         optimizer.step()
-        global_epoch_loss += loss.data[0]
+        global_epoch_loss += loss.data
         if verbose:
             if batch_idx % log_interval == 0:
                 print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                     epoch, batch_idx * len(data), len(loader.dataset), 100.
-                    * batch_idx / len(loader), loss.data[0]))
+                    * batch_idx / len(loader), loss.data))
     return global_epoch_loss / len(loader.dataset)
 
 
@@ -28,17 +28,23 @@ def test(loader, model, cuda, verbose=True):
     model.eval()
     test_loss = 0
     correct = 0
-    for data, target in loader:
-        if cuda:
-            data, target = data.cuda(), target.cuda()
-        data, target = Variable(data, volatile=True), Variable(target)
-        output = model(data)
-        test_loss += F.nll_loss(output, target, size_average=False).data[0]  # sum up batch loss
-        pred = output.data.max(1, keepdim=True)[1]  # get the index of the max log-probability
-        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+    with torch.no_grad():
+        for data, target in loader:
+            if cuda:
+                data, target = data.cuda(), target.cuda()
+            data, target = Variable(data, volatile=True), Variable(target)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, size_average=False).data  # sum up batch loss
+            pred = output.data.max(1, keepdim=True)[1]  # get the index of the max log-probability
+            correct += pred.eq(target.data.view_as(pred)).cpu().sum()
 
     test_loss /= len(loader.dataset)
     if verbose:
         print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
             test_loss, correct, len(loader.dataset), 100. * correct / len(loader.dataset)))
+    
+    #save model
+    model_save_name = 'classifier.pt'
+    path = F"/content/gdrive/My Drive/PyTorch_Classifier/{model_save_name}" 
+    torch.save(model.state_dict(), path)
     return test_loss
